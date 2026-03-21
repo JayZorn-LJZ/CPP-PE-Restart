0deb978b16a48ae8a2715fdc79be76db9511cd71
Phil-LJZ <a13926719253@163.com>
2026-03-21 09:41:14 +0800

    提交PE3.6.2

--- PE3.6/PE3.6.2/PE3.6.2.cpp
+++ PE3.6/PE3.6.2/PE3.6.2.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 int main(void)
 {
-    
+    int feet{};
+    int inches{};
+    int height{}; // height in meters
+    float weight{};
+    float bmi{};
+
+    cout << "Please enter your height in feet and inches as integers.\n";
+    cout << "Feet:___\b\b\b";
+    cin >> feet;
+    cout << "Inches:___\b\b\b";
+    cin >> inches;
+
+    cout << "Please enter your weight in pounds:___\b\b\b";
+    cin >> weight;
+
+    height = (feet * 12 + inches) * 0.0254; // Convert to meters
+    weight *= 0.453592; // Convert to kilograms
+
+    bmi = weight / (height * height); // Calculate BMI
+
+    cout << "Your BMI is: " << bmi;
 
     return 0;
 }
