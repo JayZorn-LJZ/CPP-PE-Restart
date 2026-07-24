25161f24045e9689cc62476f87be2140fb269b7b
Phil-LJZ <a13926719253@163.com>
2026-07-25 00:41:52 +0800

    完善PE6.11.5示例程序，增加收入计算及税务报告功能；新增PE6.11.6示例程序框架

--- PE6.11/PE6.11.5/main.cpp
+++ PE6.11/PE6.11.5/main.cpp
@@ -6,5 +6,20 @@ int main(void)
 {
     // 1. 读取用户的收入
     // 2. 将用户的收入根据超额累进税率划分为4份
-    // 3. 
+    // 3. 根据公式计算四份并累加进所得税变量
+    // 4. 报告所得税
+
+    double income {};
+    double shuiZongHe {};
+    cout << "Please input your income: ";
+    while (cin >> income)
+    {
+        if (income < 0)
+            break;
+        shuiZongHe = ((income > 15000)? (10000 * 0.1) : 0) + ((income > 25000)? (20000 * 0.15) : 0) + ((income > 35000)? ((income - 35000) * 0.20) : 0);
+        cout << "shuiZonghe is " << shuiZongHe << endl;
+        cout << "Please input your income: ";
+    }
+
+    return 0;
 }
