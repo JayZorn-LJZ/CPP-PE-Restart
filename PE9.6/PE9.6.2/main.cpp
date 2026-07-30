0bd8c549376328af92c2bfd87e222dd2fa5f76cf
Phil-LJZ <a13926719253@163.com>
2026-07-30 15:55:46 +0800

    新增PE9.6.2示例程序，实现使用静态局部变量的字符串计数功能

--- PE9.6/PE9.6.2/main.cpp
+++ PE9.6/PE9.6.2/main.cpp
@@ -1,1 +1,35 @@
+// static.cpp -- using a static local variable
 #include <iostream>
+using namespace std;
+
+// function prototype
+void strcount(const string & str);
+
+int main()
+{
+    string input;
+
+    cout << "Enter a line:\n";
+    getline(cin, input);
+    while (cin)
+    {
+        if (input == "")
+            return 0;
+        strcount(input);
+        cout << "Enter next line (empty line to quit):\n";
+        getline(cin, input);
+    }
+    cout << "Bye\n";
+    return 0;
+}
+
+void strcount(const string & str)
+{
+    using namespace std;
+    static int total = 0;        // static local variable
+
+    cout << "\"" << str << "\" contains ";
+    total += str.size();
+    cout << str.size() << " characters\n";
+    cout << total << " characters total\n";
+}
