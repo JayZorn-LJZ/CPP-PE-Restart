f603a5a6da7ef6182f30c882a8c1868ddce601a1
Phil-LJZ <a13926719253@163.com>
2026-08-02 11:23:22 +0800

    新增 main.cpp 和 person.h 文件，定义基本结构和类 Person

---
+++ PE10.8/PE10.8.2/person.h
@@ -0,0 +1,22 @@
+#ifndef PERSON_H_
+#define PERSON_H_
+#include <iostream>
+
+namespace PERSON
+{
+    // const int LIMIT = 25; // 下面的常量的替代方案
+    class Person
+    {
+        static const int LIMIT = 25;
+        string lname;
+        char fname[LIMIT];
+        // enum limit {LIMIT = 25}; // 常量替代方案
+        public:
+            Person() {lname = "";fname[0] = '\0';}
+            Person(const string &, const char * fn = "Heyyou");
+            void show() const;
+            void formalshow() const;
+    };
+}
+
+#endif
